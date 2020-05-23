68f4047680d9c539f957c3f87101fd0ef9246042
futurejones <futurejones@users.noreply.github.com>
2020-05-24 07:02:30 +0900

    Create build_swift.sh

---
+++ Swift-5.1.5-ci/build_swift.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+today=`date +%Y-%m-%d`
+REL=5.1.5
+INSTALL_DIR=/home/install
+PACKAGE=/home/output/swift-${REL}-RELEASE-armv6-raspbian-buster_$today.tar.gz
+
+# run update-checkout to create symlink for LLVM Project
+#./swift-source/swift/utils/update-checkout --tag swift-5.1.5-RELEASE
+
+./swift-source/swift/utils/build-script \
+    -R \
+    --build-subdir buildbot_linux --install-prefix=/usr \
+    --install-destdir=${INSTALL_DIR} --installable-package=${PACKAGE} \
+    --lldb --llbuild --xctest --foundation --libdispatch \
+    -- --install-libdispatch --install-foundation --install-swift --install-lldb --install-llbuild --install-xctest \
+    '--swift-install-components=autolink-driver;compiler;clang-builtin-headers;stdlib;swift-remote-mirror;sdk-overlay;dev' \
+    --build-swift-static-stdlib --build-swift-static-sdk-overlay
